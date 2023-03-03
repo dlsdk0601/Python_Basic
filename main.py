76be36d3e07217d3ce67cd1ff3684ef1747a9972
dlsdk0601 <dlsdk0601@gmail.com>
2023-03-04 01:03:55 +0900

    feat: function parameter

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def app():
-    def say_hello():
-        print("Hello World")
+    def say_hello(user_name, user_age):
+        print(f"Hello, {user_name} how r u? your age is {user_age}")
+        print("Hello", user_name, "how r u? your age is", user_age)
 
-    say_hello()
+    say_hello("ina", 30)
+    say_hello(user_name="ina", user_age=20)
 
 
 if __name__ == '__main__':
