bd88bdff21e37d53818d0e440cdfa9add1e84aab
dlsdk0601 <dlsdk0601@gmail.com>
2023-03-10 21:01:37 +0900

    feat: list

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 def app():
-    # days_of_week = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
-    # print(days_of_week)
-
-    name = "ina"
-    print(name.upper())
-    print(name.lower())
-    print(name.capitalize())
-    print(name.startswith("i"))  # True
-    print(name.replace("a", "z"))  # inz
-
+    days_of_week = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+    print(days_of_week)
+    print(days_of_week[1])  # tue
+    days_of_week.count("wed")  # wed 와 같은 값은 몇개 있는지
+    days_of_week.clear()  # []
+    days_of_week.reverse()  # 반대로 순서 변경
+    days_of_week.append("ddd")  # ddd 값 추가
+    days_of_week.remove("mon")  # mon 삭제
 
 
 if __name__ == '__main__':
