31c846180cfa35efc1c32121801c69a9b65881d2
dlsdk0601 <dlsdk0601@gmail.com>
2023-03-10 20:56:58 +0900

    feat: string 내장 함수들

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from random import randint
-
-
 def app():
-    pc_choice = randint(1, 50)
-    playing = True
+    # days_of_week = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+    # print(days_of_week)
 
-    while playing:
-        user_choice = int(input("Choose number."))
-        if user_choice == pc_choice:
-            print("승")
-            playing = False
-        elif user_choice > pc_choice:
-            print("아래")
-        elif user_choice < pc_choice:
-            print("위")
+    name = "ina"
+    print(name.upper())
+    print(name.lower())
+    print(name.capitalize())
+    print(name.startswith("i"))  # True
+    print(name.replace("a", "z"))  # inz
+
 
 
 if __name__ == '__main__':
