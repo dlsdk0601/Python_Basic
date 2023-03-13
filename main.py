32a415756e39867421e39a5bb9153ff920dfb6cd
dlsdk0601 <dlsdk0601@gmail.com>
2023-03-14 01:08:21 +0900

    feat: 필요한 데이터만 추출해서 list에 추가

--- main.py
+++ main.py
@@ -10,17 +10,27 @@ def app():
     if res.status_code != 200:
         print("Can not request website")
     else:
+        results = []
         print("res")
         soup = BeautifulSoup(res.text, "html.parser")
         jobs = soup.find_all("section", class_="jobs")
-        print(jobs)
-        print(len(jobs))
         for section in jobs:
             job_posts = section.find_all("li")
             job_posts.pop(-1)  # view_all 태그 지우기
             for post in job_posts:
-                print(post)
-                print("::::::")
+                anchors = post.find_all("a")
+                anchor = anchors[1]
+                link = anchor["href"]
+                company, kind, region = anchor.find_all("span", class_="company")
+                title = anchor.find("span", class_="title")
+                job_date = {
+                    'company': company.string,
+                    'region': region.string,
+                    'position': title.string
+                }
+                results.append(job_date)
+
+        print(results)
 
 
 if __name__ == '__main__':
