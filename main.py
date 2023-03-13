deb3f1330926c5e02f221dabdf4783209ade4b5f
dlsdk0601 <dlsdk0601@gmail.com>
2023-03-14 00:51:16 +0900

    feat: li 태그만 뽑아서 추출

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ def app():
         soup = BeautifulSoup(res.text, "html.parser")
         jobs = soup.find_all("section", class_="jobs")
         print(jobs)
+        print(len(jobs))
+        for section in jobs:
+            job_posts = section.find_all("li")
+            job_posts.pop(-1)  # view_all 태그 지우기
+            for post in job_posts:
+                print(post)
+                print("::::::")
 
 
 if __name__ == '__main__':
