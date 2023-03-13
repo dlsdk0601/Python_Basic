7740aa7051d85f99917b4c8431df63879efb330e
dlsdk0601 <dlsdk0601@gmail.com>
2023-03-13 23:39:53 +0900

    feat: pip install requests and get method

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ def app():
         "facebook.com"
     )
 
+    results = {}
+
     for website in websites:
         if not website.startswith("https://"):
             print("fixed")
             website = f'https://{website}'
 
+        res = get(website)
+        print("res")
+        print(res)
+        print(res.status_code)
+        if res.status_code == 200:
+            print(f'{website} is Ok')
+            results[website] = "OK"
+        else:
+            print(f'{website}is not Ok')
+            results[website] = "FAIL"
 
 
 if __name__ == '__main__':
