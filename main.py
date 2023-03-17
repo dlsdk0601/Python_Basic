70f8e9b76715f4df19fea375344ef2c90631e6f4
dlsdk0601 <dlsdk0601@gmail.com>
2023-03-17 20:54:27 +0900

    feat: indeed 크롤링 추가

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ options.add_argument("--disable-dev-shm-usage")
 
 browser = webdriver.Chrome(options=options)
 
+
 def app():
     jobs = extract_wwr_jobs("python")
     base_url = "https://kr.indeed.com/jobs?q="
@@ -21,12 +22,27 @@ def app():
     if response.status_code != 200:
         print("bad")
     else:
+        results = []
         soup = BeautifulSoup(response.text, "html.parser")
         job_list = soup.find("ul", clss_="jobsearch-ResultsList")
         jobs = job_list.find_all("li", recursive=False)
         for job in jobs:
-            print(job)
-        print("good")
+            zone = job.find("div", class_="mosaic-zone")
+            if zone is None:
+                anchor = job.select("h2 a")
+                title = anchor["aria-label"]
+                link = anchor["href"]
+                company = job.find("span", class_="companyName")
+                location = job.find("div", class_="companyLocation")
+                job_data = {
+                    'link': f"https://kr.indeed.com/{link}",
+                    'company': company.string,
+                    'location': location.string,
+                    'position': title,
+                }
+                results.append(job_data)
+        for result in results:
+            print(result, "\n/////\n")
 
 
 if __name__ == '__main__':
