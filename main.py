a7421091a676ab36a137ed864a14eebbdba1b8e5
dlsdk0601 <dlsdk0601@gmail.com>
2023-03-18 00:11:15 +0900

    feat: pagination 읽어서 5 페이지만 크롤링 할 수 있게 리팩토링

--- main.py
+++ main.py
@@ -12,37 +12,62 @@ options.add_argument("--disable-dev-shm-usage")
 browser = webdriver.Chrome(options=options)
 
 
-def app():
-    jobs = extract_wwr_jobs("python")
+def get_page_count(keyword):
     base_url = "https://kr.indeed.com/jobs?q="
-    keyword = "python"
 
-    response = browser.get(f"{base_url}{keyword}")
+    response = get(f"{base_url}{keyword}")
 
     if response.status_code != 200:
-        print("bad")
+        return 0
     else:
-        results = []
         soup = BeautifulSoup(response.text, "html.parser")
-        job_list = soup.find("ul", clss_="jobsearch-ResultsList")
-        jobs = job_list.find_all("li", recursive=False)
-        for job in jobs:
-            zone = job.find("div", class_="mosaic-zone")
-            if zone is None:
-                anchor = job.select("h2 a")
-                title = anchor["aria-label"]
-                link = anchor["href"]
-                company = job.find("span", class_="companyName")
-                location = job.find("div", class_="companyLocation")
-                job_data = {
-                    'link': f"https://kr.indeed.com/{link}",
-                    'company': company.string,
-                    'location': location.string,
-                    'position': title,
-                }
-                results.append(job_data)
-        for result in results:
-            print(result, "\n/////\n")
+        pagination = soup.find("ul", class_="pagination-list")
+        if pagination is None:
+            return 1
+        else:
+            pages = pagination.find_all("li", recursive=False)
+            count = len(pages)
+            if count >= 5:
+                return 5
+            else:
+                return count
+
+
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
+    for page in range(pages):
+        base_url = "https://kr.indeed.com/jobs?q="
+
+        response = get(f"{base_url}{keyword}")
+
+        if response.status_code != 200:
+            print("bad")
+        else:
+            results = []
+            soup = BeautifulSoup(response.text, "html.parser")
+            job_list = soup.find("ul", clss_="jobsearch-ResultsList")
+            jobs = job_list.find_all("li", recursive=False)
+            for job in jobs:
+                zone = job.find("div", class_="mosaic-zone")
+                if zone is None:
+                    anchor = job.select("h2 a")
+                    title = anchor["aria-label"]
+                    link = anchor["href"]
+                    company = job.find("span", class_="companyName")
+                    location = job.find("div", class_="companyLocation")
+                    job_data = {
+                        'link': f"https://kr.indeed.com/{link}",
+                        'company': company.string,
+                        'location': location.string,
+                        'position': title,
+                    }
+                    results.append(job_data)
+            for result in results:
+                print(result, "\n/////\n")
+
+
+def app():
+    extract_indeed_jobs()
 
 
 if __name__ == '__main__':
